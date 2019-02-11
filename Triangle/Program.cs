d2ceca4cb948b34096b6eb0d035cdde91431aada
Lev# <mr.lmartirosyan888@gmail.com>
2019-02-11 20:08:19 +0400

    relise one

---
+++ Triangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If you want to draw triangle");
+            Console.Write("Write your first digit");
+            Console.Write(" Height = ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Write your second digit");
+            Console.Write(" Width = ");
+            int z = Convert.ToInt32(Console.ReadLine());
+            int y = 0;
+
+
+            for (int i = 0; i < x ; i++)
+            {
+                for (int r = 0; r < (y = z); r++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
